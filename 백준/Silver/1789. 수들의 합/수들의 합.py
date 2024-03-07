ede1b3b37e218277e8f95bebacf9b192ce74df7c
pengcon <88936374+pengcon@users.noreply.github.com>
2024-03-07 17:09:50 +0900

    [Silver V] Title: 수들의 합, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,12 +1,14 @@
+def binary_search(start,end,n,answer):
+    if start>end:
+        return answer
+    mid=(start+end)//2
+    sum=(mid*(mid+1))//2
+    if sum==n:
+        answer=mid
+        return answer
+    elif sum<n:
+        return binary_search(mid+1,end,n,mid)
+    elif sum>n:
+        return binary_search(start,mid-1,n,answer)
 n=int(input())
-sum=0
-
-i=0
-while(True):
-    i+=1
-    sum=sum+i
-
-    if sum>n:
-       
-        print(i-1)
-        break
+print(binary_search(1,n,n,0))
