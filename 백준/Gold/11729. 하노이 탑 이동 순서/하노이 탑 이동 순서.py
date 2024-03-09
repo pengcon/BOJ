917bd3a93563090fb52e4e3d19b949db8a2532e1
pengcon <88936374+pengcon@users.noreply.github.com>
2024-03-09 22:05:32 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 1744 ms, Memory: 72092 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -0,0 +1,24 @@
+def move(num,start,arrive):
+    temp_list=[]
+    if num>1:
+        for i in range(1,4):
+            if i!=start and i!=arrive:
+                temp_arrive=i
+        a=move(num-1,start,temp_arrive)
+        b=move(1,start,arrive)
+        temp_list.append(b)
+        c=move(num-1,temp_arrive,arrive)
+        temp_list=a+b+c
+    else:
+      
+        return [start,arrive]
+    return temp_list
+
+n=int(input())
+answer_list=(move(n,1,3))
+print(len(answer_list)//2)
+for i in range(len(answer_list)//2):
+    print(answer_list[2*i],end=' ')
+    print(answer_list[2*i+1])
+# for i in range(len(answer_list)):
+    # print(str(answer_list[i][0])+" "+str(answer_list[i][1]))
