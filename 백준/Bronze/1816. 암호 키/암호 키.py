b6239e1f644accde6ea9e934aa6ad751c64ee301
pengcon <88936374+pengcon@users.noreply.github.com>
2024-03-14 16:43:59 +0900

    [Bronze I] Title: 암호 키, Time: 252 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -0,0 +1,11 @@
+t= int(input())
+
+for _ in range(t):
+    num=int(input())
+    
+    for i in range(2,1_000_001):
+        if num%i==0:
+            print("NO")
+            break
+        if i == 1_000_000:
+            print("YES")
