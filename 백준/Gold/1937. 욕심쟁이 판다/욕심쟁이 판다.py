94206d1749a502056a1bb06dbd063571799548dd
pengcon <88936374+pengcon@users.noreply.github.com>
2024-03-28 16:34:09 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 388 ms, Memory: 183200 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,31 +1,30 @@
-from collections import deque
-import sys
-input=sys.stdin.readline
-n=int(input())
-bamboo_list=[]
-max_move=0
-for i in range(n):
-    temp_list=list(map(int,input().split()))
-    bamboo_list.append(temp_list)
-
-dp=[[0 for i in range(n)] for j in range(n)]  
-
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
-
 def dfs(x,y):
-    if dp[x][y]:
+    if dp[x][y]!=0:
         return dp[x][y]
-    dp[x][y]=1
-    # 현재 노드와 연결된 다른 노드를 재귀적(스택 기반)으로 방문
     for i in range(4):
         nx=x+dx[i]
         ny=y+dy[i]
-        if 0<=nx<n and 0<=ny<n and bamboo_list[x][y]<bamboo_list[nx][ny]:
-            dp[x][y]=max(dp[x][y],dfs(nx,ny)+1)
+        
+        if 0<=nx<n and 0<=ny<n:
+            if graph[x][y] < graph[nx][ny]:
+                dp[x][y]=max(dp[x][y],dfs(nx,ny)+1)
     return dp[x][y]
+    
+n= int(input())
 
-for i in range(n):
-    for j in range(n):             
-        max_move=max(max_move,dfs(i,j))
-print(max_move)
+graph = [list(map(int,input().split())) for _ in range(n)]
+
+dp = [[0 for _ in range(n)]for _ in range(n)]
+#모든 점 방문
+
+#방문한 뒤에 이동할 수 있는 모둔 경우의 수를 재귀로 구현
+
+#재귀 구현 뒤 dp로 바꾼다.
+
+for x in range(n):
+    for y in range(n):
+        dfs(x,y)
+# print(dp)
+print(max(map(max,dp))+1)
