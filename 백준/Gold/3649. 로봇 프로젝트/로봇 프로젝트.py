946427f5b9799172f6fe829179369f191ccf57b1
pengcon <88936374+pengcon@users.noreply.github.com>
2024-04-05 12:22:03 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 2720 ms, Memory: 221644 KB -BaekjoonHub

---
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.py
@@ -0,0 +1,50 @@
+import sys
+input=sys.stdin.readline
+
+while True:
+    try:
+        X=int(input())*10_000_000
+        N=int(input())
+        legos=[]
+        for _ in range(N):
+            legos.append(int(input()))
+        legos.sort()
+
+        start=0 
+        end=N-1 
+
+        # # end 구하기 
+        # while(start<=end):
+        #     mid=(start+end)//2 
+        #     #부등호는 생각해보기
+        #     if legos[mid]>=X:
+        #         end=mid-1
+        #     else:
+        #         start=mid+1
+        flag=True
+        start=0
+        while(start<end): 
+            # print(start,end)
+            if legos[start]+legos[end]==X: 
+                print("yes "+str(legos[start])+" "+str(legos[end]))
+                flag =False
+                break 
+            elif legos[start]+legos[end]>X: 
+                end-=1 
+            else:
+                start+=1
+        if flag:
+                print("danger")
+    except:
+        break
+
+
+
+
+
+
+
+
+
+
+
