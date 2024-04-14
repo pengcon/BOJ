41daf580fef5214da8f31fa94ec3c340172619d9
pengcon <88936374+pengcon@users.noreply.github.com>
2024-04-14 23:49:26 +0900

    [Silver I] Title: 효율적인 해킹, Time: 16820 ms, Memory: 214620 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+
+N,M=map(int,input().split())
+link=[ []for i in range(N+1)]
+
+for _ in range(M):
+    B,A=map(int,input().split())
+    link[A].append(B)
+
+
+def bfs(i):
+    visited=set()
+    temp_max=0
+    q=deque([i])
+    visited.add(i)
+    while q:
+        node=q.popleft()
+        for j in link[node]:
+            if j not in visited:
+                visited.add(j)
+                q.append(j)
+                temp_max+=1
+    return temp_max
+
+ans=[]
+for i in range(1,N+1):
+    ans.append(bfs(i))
+
+high=max(ans)
+for k in range(N):
+    if ans[k]==high:
+        print(k+1,end=" ") 
