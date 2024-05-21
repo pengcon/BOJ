229b310ec88ec60652e9aa1720fe6a3d7731d21b
pengcon <88936374+pengcon@users.noreply.github.com>
2024-05-21 15:34:07 +0900

    [Silver I] Title: 오목, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -49,28 +49,67 @@ def diagonal_up(num,x,y,count):
         return diagonal_up(num,x-1,y+1,count+1)
     elif table[x][y]!=num:
         return count
+    
+def filter_count_not_six_for_garo(i,j):
+    if j-1==-1 or table[i][j]!=table[i][j-1]:
+        return True
+    else:
+        return False
+def filter_count_five_for_garo(i,j):
+    if garo(table[i][j],i,j+1,1)==5:
+        return filter_count_not_six_for_garo(i,j)
+
+
+def filter_count_not_six_for_sero(i,j):
+    if i-1==-1 or table[i][j]!=table[i-1][j]:
+        return True
+    else:
+        return False
+def filter_count_five_for_sero(i,j):
+    if sero(table[i][j],i+1,j,1)==5:
+        return filter_count_not_six_for_sero(i,j)
+
+
+
+def filter_count_not_six_for_diagonal_down(i,j):
+    if i-1==-1 or j-1==-1 or table[i][j]!=table[i-1][j-1] :
+        return True
+    else:
+        return False
+    
+def filter_count_five_for_diagonal_down(i,j):
+    if diagonal_down(table[i][j],i+1,j+1,1)==5:
+        return filter_count_not_six_for_diagonal_down(i,j)
+
+
+def filter_count_not_six_for_diagonal_up(i,j):
+    if i+1==len_table or j-1==-1 or table[i][j]!=table[i+1][j-1]:
+        return True
+    else:
+        return False
+    
+def filter_count_five_for_diagonal_up(i,j):
+    if diagonal_up(table[i][j],i-1,j+1,1)==5:
+        return filter_count_not_six_for_diagonal_up(i,j)
+
 
 find_ans=False
 for i in range(len_table):
     for j in range(len_table):
         if table[i][j]!=0:
-            if garo(table[i][j],i,j+1,1)==5:
-                if j-1==-1 or table[i][j]!=table[i][j-1]:
+            if filter_count_five_for_garo(i,j):
+                print_ans(i,j)
+                find_ans = True
+                break
+            elif filter_count_five_for_sero(i,j):
+                print_ans(i,j)
+                find_ans = True
+                break
+            elif filter_count_five_for_diagonal_down(i,j):
                     print_ans(i,j)
                     find_ans = True
                     break
-            elif sero(table[i][j],i+1,j,1)==5:
-                if i-1==-1 or table[i][j]!=table[i-1][j]:
-                    print_ans(i,j)
-                    find_ans = True
-                    break
-            elif diagonal_down(table[i][j],i+1,j+1,1)==5:
-                if i-1==-1 or j-1==-1 or table[i][j]!=table[i-1][j-1] :
-                    print_ans(i,j)
-                    find_ans = True
-                    break
-            elif diagonal_up(table[i][j],i-1,j+1,1)==5:
-                if i+1==len_table or j-1==-1 or table[i][j]!=table[i+1][j-1] :
+            elif filter_count_five_for_diagonal_up(i,j):
                     print_ans(i,j)
                     find_ans = True
                     break
