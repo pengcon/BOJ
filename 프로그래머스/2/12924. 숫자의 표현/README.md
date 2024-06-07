82faa30ba2af5e655bf119c8b43c44116de3c509
pengcon <88936374+pengcon@users.noreply.github.com>
2024-06-07 20:04:07 +0900

    [level 2] Title: 숫자의 표현, Time: 4.99 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 4.98 ms
+메모리: 10.1 MB, 시간: 4.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 07일 19:59:49
+2024년 06월 07일 20:04:06
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,13 +1,18 @@
 def solution(n):
     answer = 0
+    #완탐
     
-    
+    #1부터 n까지
     for i in range(1,n+1):
         hap=0
+        #i부터 n까지
         for j in range(i,n+1):
+            #연속된 자연수들을 더해감 (i+(i+1)+(i+2) ...)
             hap = hap + j
+            # 합이 n보다 크면 for문을 중단하고 다음수부터 시작 ((i+1)+(i+2)+(i+3)...)
             if hap>n:
                 break
+            # 합과 n이 같으면 정답을 증가시켜줌
             elif hap==n:
                 answer += 1
     return answer
