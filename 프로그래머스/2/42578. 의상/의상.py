19ff7b11076bc0b8abaedf54cbc1ca98276d80f7
pengcon <88936374+pengcon@users.noreply.github.com>
2024-06-18 22:35:11 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+from itertools import combinations
+def solution(clothes):
+    answer = 1
+    wear = defaultdict(int)
+    for i in clothes:
+        wear[i[1]] += 1
+    w = list(wear.keys())
+    
+    for i in w:
+        answer = answer * (wear[i]+1) 
+    
+    
+    
+    return answer-1
