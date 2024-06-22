626f1e048cbe5a763dbf96b6da04d8c1ecfe0160
pengcon <88936374+pengcon@users.noreply.github.com>
2024-06-22 15:30:30 +0900

    [Silver V] Title: 올림픽, Time: 276 ms, Memory: 26528 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.kt
@@ -0,0 +1,51 @@
+import java.io.*
+fun main() = with(System.`in`.bufferedReader()){
+    val (n,k) = readLine().split(" ").map{it.toInt()}
+    val foreignList = mutableListOf<IntArray>()
+
+    repeat(n){
+        val foreign = readLine().split(" ").map{it.toInt()}
+        foreignList.add(intArrayOf(foreign[0],foreign[1],foreign[2],foreign[3],0))
+
+    }
+    foreignList.sortWith (compareBy(
+        {-it[1]},
+        {-it[2]},
+        {-it[3]}
+    ) )
+
+    var rank = 1
+    var temp = 0
+    foreignList[0][4] = rank
+    for (i in (0..<foreignList.size-1)){
+        if (foreignList[i][1] > foreignList[i+1][1]){
+            rank += 1
+            foreignList[i+1][4] = rank + temp
+            temp = 0
+        }
+        else if (foreignList[i][1] == foreignList[i+1][1]){
+            if (foreignList[i][2] > foreignList[i+1][2]) {
+                rank += 1
+                foreignList[i+1][4] = rank + temp
+                temp = 0
+
+            }
+            else if (foreignList[i][2] == foreignList[i+1][2]){
+                if (foreignList[i][3] > foreignList[i+1][3]){
+                    rank += 1
+                    foreignList[i+1][4] = rank + temp
+                    temp = 0
+                }
+                else if (foreignList[i][3] == foreignList[i+1][3]){
+                    foreignList[i+1][4] = rank
+                    temp += 1
+                }
+            }
+        }
+    }
+    for (i in 0..<foreignList.size){
+        if (foreignList[i][0] == k){
+            println(foreignList[i][4])
+        }
+    }
+}
