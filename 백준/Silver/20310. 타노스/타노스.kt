ea9327a2ed7a18a1337520b7e459d1f8e8501492
pengcon <88936374+pengcon@users.noreply.github.com>
2024-07-01 22:59:02 +0900

    [Silver III] Title: 타노스, Time: 88 ms, Memory: 12488 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.kt
+++ 백준/Silver/20310. 타노스/타노스.kt
@@ -1,15 +1,33 @@
 fun main() {
-    val s = readln()
-    val zeroNum = s.count{it == '0'}
-    val oneNum = s.count{it == '1'}
+    val st = readln()
+    val arr = ArrayList<Char>()
+    for (s in st){
+        arr.add(s)
+
+    }
+    val zeroNum = st.count{it == '0'}
+    val oneNum = st.count{it == '1'}
     val halfZeroNum = zeroNum / 2
     val halfOneNum = oneNum / 2
-    var new = ""
+
     for (i in 1..halfZeroNum){
-        new += '0'
+        for (idx in arr.indices.reversed()){
+            if ( (arr[idx]) == '0' ){
+                arr.removeAt(idx)
+                break
+            }
+        }
     }
+
     for (i in 1..halfOneNum){
-        new += '1'
+        for (idx in arr.indices){
+            if ( (arr[idx]) == '1' ){
+                arr.removeAt(idx)
+                break
+            }
+        }
     }
-    print(new)
+    for (a in arr){
+        print(a)
+    }
 }
