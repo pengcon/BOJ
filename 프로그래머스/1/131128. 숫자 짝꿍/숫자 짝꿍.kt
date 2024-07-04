fc60b19882de0f5d393144071d89dfd58a48a905
pengcon <88936374+pengcon@users.noreply.github.com>
2024-07-05 00:57:54 +0900

    [level 1] Title: 숫자 짝꿍, Time: 12.14 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
@@ -0,0 +1,60 @@
+class Solution {
+    fun solution(X: String, Y: String): String {
+        var answer: String = ""
+        var answerList = List<Int>(1){1}
+        val xMap = hashMapOf('0' to 0,
+                            '1' to 0,
+                            '2' to 0,
+                            '3' to 0,
+                            '4' to 0,
+                            '5' to 0,
+                            '6' to 0,
+                            '7' to 0,
+                            '8' to 0,
+                            '9' to 0)
+        val yMap = hashMapOf('0' to 0,
+                            '1' to 0,
+                            '2' to 0,
+                            '3' to 0,
+                            '4' to 0,
+                            '5' to 0,
+                            '6' to 0,
+                            '7' to 0,
+                            '8' to 0,
+                            '9' to 0)
+    for (xc in X){
+
+        xMap[xc] = xMap[xc]!! + 1 
+    }     
+    for (yc in Y){
+        yMap[yc] = yMap[yc]!! + 1 
+    } 
+    var tempAns = ArrayList<Int>()
+    var flag = true
+    for (key in xMap.keys){
+        // print("${key}, ${xMap[key]}, ${yMap[key]} ")
+        var count = 0
+        if (xMap[key]!! > yMap[key]!!){
+            count = yMap[key]!!
+        } else count = xMap[key]!!
+        if (key != '0' && count > 0){
+            flag = false
+        }
+        for (i in 1..count){
+            tempAns.add(key.toString().toInt())
+        }
+    }
+    answerList = tempAns.sortedDescending()
+    
+    answer = answerList.joinToString("")
+    if (answer == ""){
+        answer = "-1"
+    }
+    else if (flag){
+        answer = "0"
+        
+    } else answer = answer.toString()
+    return answer
+    }
+
+}
