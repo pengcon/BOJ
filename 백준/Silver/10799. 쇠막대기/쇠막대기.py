6a94b4a09cf207f1597c603d94bbbff60e3a0893
Lee Hoon <88936374+pengcon@users.noreply.github.com>
2024-12-15 22:24:33 +0900

    [Silver II] Title: 쇠막대기, Time: 132 ms, Memory: 115540 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+dq = deque()
+
+st = input()
+
+ans = 0
+for i in range(len(st)):
+    if st[i]=='(':
+        dq.append(st[i])
+    elif st[i]==')':
+        if st[i-1]=='(':
+            dq.pop()
+            ans += len(dq)
+        elif st[i-1]==')':
+            dq.pop()
+            ans += 1
+print(ans)
+        
