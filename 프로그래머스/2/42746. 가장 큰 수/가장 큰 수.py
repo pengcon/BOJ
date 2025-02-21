25137414583246d5bc1502a090a700b4aebb2cda
Lee Hoon <88936374+pengcon@users.noreply.github.com>
2025-02-21 18:01:53 +0900

    [level 2] Title: 가장 큰 수, Time: 83.10 ms, Memory: 27 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    numbers = list(map(str,numbers))
+    numbers.sort(key=lambda x : x*3,reverse=True)
+    answer = ''
+    for i in numbers:
+        answer += i
+    if int(numbers[0]) == 0:
+        return "0"
+    else:
+        return answer
